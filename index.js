3967dea52e6c878250e382d3e4be08a21270ed1c
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-05-09 19:41:11 +0530

    small fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function swapnoneelInfo() {
   const intro =
-    "Hello, I'm Swapnoneel Saha, an Web Developer, UI/UX designer and Technical Writer based in Kolkata, India.";
+    "Hello, I'm Swapnoneel Saha, a Web Developer, UI/UX designer, and Technical Writer based in Kolkata, India.";
   const work =
-    "Currently, I'm  working as a Technical Writer at Tutorials Point.";
+    "Currently, I'm working as a Technical Writer at Tutorials Point.";
   const website = "https://swapnoneel.vercel.app";
   const twitter = "https://twitter.com/swapnoneel123";
   const github = "https://github.com/Swpn0neel";
@@ -10,7 +10,7 @@ function swapnoneelInfo() {
   const leetcode = "https://leetcode.com/u/Swapnoneel/";
   const email = "swapnoneelsaha111@gmail.com";
   const contact =
-    "If you want to contact me, consider dropping me a mail at swapnoneelsaha111@gmail.com";
+    "If you want to contact me, consider dropping me an email at swapnoneelsaha111@gmail.com";
   const bye = "Bye! Have a nice day!!";
 
   const message = `
@@ -23,8 +23,8 @@ ${work}\x1b[38;5;51m
 \x1b[38;5;93mGitHub:\x1b[0m \x1b[38;5;39m${github}\x1b[38;5;51m
 \x1b[38;5;93mLeetcode:\x1b[0m \x1b[38;5;39m${leetcode}\x1b[38;5;51m
 
-\x1b[38;5;105mIf you want to contact me, consider dropping me a mail at\x1b[0m \x1b[38;5;39m${email}\x1b[38;5;51m
-\x1b[38;5;105m${bye}\x1b\x1b[0m`;
+\x1b[38;5;105mIf you want to contact me, consider dropping me an email at\x1b[0m \x1b[38;5;39m${email}\x1b[38;5;51m
+\x1b[38;5;105m${bye}\x1b[0m`;
 
   console.log(message);
 }
